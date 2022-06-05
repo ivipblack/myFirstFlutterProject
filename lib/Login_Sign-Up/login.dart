144e13223379e1ae4f752330d80c476e70305a23
Jawad <jawad_1422@hotmail.com>
2022-06-05 11:31:07 +0300

    fixed login column overflow

--- lib/Login_Sign-Up/login.dart
+++ lib/Login_Sign-Up/login.dart
@@ -41,208 +41,211 @@ class _LoginViewState extends State<LoginView> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.fromLTRB(unit * 2, unit, unit * 2, unit),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: unitHeightValue * 20,
-              height: unitHeightValue * 25,
-              child: SvgPicture.asset('publicAssets/images/realm.svg'),
-            ),
-            Text(
-              'Welcome to Vacation!!',
-              style: TextStyle(fontSize: unitHeightValue * 4),
-            ),
-            SizedBox(
-              height: unitHeightValue * 4,
-            ),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(unit * 1)),
-                  labelText: 'Email',
-                  contentPadding: EdgeInsets.all(unit * 2),
-                  prefixIcon: const Icon(Icons.email)),
-            ),
-            SizedBox(
-              height: unitHeightValue * 2,
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: _isHiddenPassword,
-              decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(unit * 2),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(1 * unit)),
-                  labelText: 'Password',
-                  prefixIcon: const Icon(Icons.security),
-                  suffixIcon: InkWell(
-                    onTap: _viewPassword,
-                    child: eyeIcon,
-                  )),
-            ),
-            SizedBox(
-              height: unitHeightValue * 2,
-            ),
-            // ignore: sized_box_for_whitespace
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  height: unit * 6,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                    ),
-                    onPressed: () {
-                      signIn();
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //       builder: (context) => const HomeScreen()),
-                      // );
-                    },
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: unitHeightValue * 2),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: unitHeightValue * 1.5,
-            ),
-            Text(
-              'Forgot password?',
-              style: TextStyle(fontSize: unitHeightValue * 1.5),
-            ),
-            // Divider(),
-            SizedBox(
-              height: unitHeightValue * 3,
-            ),
-            GestureDetector(
-                onTap: () {},
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: unitHeightValue * 20,
-                      height: unitHeightValue * 7.5,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomeScreen()),
-                          );
-                        },
-                        // : Image.asset('publicAssets/images/google_btn.png'),
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.fromLTRB(0, 0, unit * 0.6, 0),
-                              width: unit * 3,
-                              height: unit * 3,
-                              child: SvgPicture.asset(
-                                'publicAssets/images/google.svg',
-                              ),
-                            ),
-                            Text(
-                              'Sign in with Google',
-                              style: TextStyle(
-                                fontSize: unit * 1.3,
-                                color: Colors.black87,
-                              ),
-                            ),
-                          ],
-                        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: unitHeightValue * 20,
+                height: unitHeightValue * 25,
+                child: SvgPicture.asset('publicAssets/images/realm.svg'),
+              ),
+              Text(
+                'Welcome to Vacation!!',
+                style: TextStyle(fontSize: unitHeightValue * 4),
+              ),
+              SizedBox(
+                height: unitHeightValue * 4,
+              ),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(unit * 1)),
+                    labelText: 'Email',
+                    contentPadding: EdgeInsets.all(unit * 2),
+                    prefixIcon: const Icon(Icons.email)),
+              ),
+              SizedBox(
+                height: unitHeightValue * 2,
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: _isHiddenPassword,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(unit * 2),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(1 * unit)),
+                    labelText: 'Password',
+                    prefixIcon: const Icon(Icons.security),
+                    suffixIcon: InkWell(
+                      onTap: _viewPassword,
+                      child: eyeIcon,
+                    )),
+              ),
+              SizedBox(
+                height: unitHeightValue * 2,
+              ),
+              // ignore: sized_box_for_whitespace
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    height: unit * 6,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black,
+                      ),
+                      onPressed: () {
+                        signIn();
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //       builder: (context) => const HomeScreen()),
+                        // );
+                      },
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(fontSize: unitHeightValue * 2),
                       ),
                     ),
-                    const SizedBox(
-                      width: 18,
-                    ),
-                    Container(
-                      width: unitHeightValue * 20,
-                      height: unitHeightValue * 7.5,
-                      child: ElevatedButton.icon(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.black,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomeScreen()),
-                          );
-                        },
-                        icon: const Icon(Icons.apple),
-                        label: Text(
-                          'Sign in with Apple',
-                          style: TextStyle(
-                            fontSize: unitHeightValue * 1.3,
-                            color: Colors.white70,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: unitHeightValue * 1.5,
+              ),
+              Text(
+                'Forgot password?',
+                style: TextStyle(fontSize: unitHeightValue * 1.5),
+              ),
+              // Divider(),
+              SizedBox(
+                height: unitHeightValue * 3,
+              ),
+              GestureDetector(
+                  onTap: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: unitHeightValue * 20,
+                        height: unitHeightValue * 7.5,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomeScreen()),
+                            );
+                          },
+                          // : Image.asset('publicAssets/images/google_btn.png'),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin:
+                                    EdgeInsets.fromLTRB(0, 0, unit * 0.6, 0),
+                                width: unit * 3,
+                                height: unit * 3,
+                                child: SvgPicture.asset(
+                                  'publicAssets/images/google.svg',
+                                ),
+                              ),
+                              Text(
+                                'Sign in with Google',
+                                style: TextStyle(
+                                  fontSize: unit * 1.3,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                )),
-            SizedBox(
-              height: unit * 2,
-            ),
-            RichText(
-              text: TextSpan(
-                text: 'Don\'t have an account? ',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: unit * 2.3,
-                ),
-                children: [
-                  TextSpan(
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = widget.onClickedSignup,
-                    text: 'Sign Up',
-                    style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Theme.of(context).colorScheme.secondary),
-                  )
-                ],
+                      const SizedBox(
+                        width: 18,
+                      ),
+                      Container(
+                        width: unitHeightValue * 20,
+                        height: unitHeightValue * 7.5,
+                        child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomeScreen()),
+                            );
+                          },
+                          icon: const Icon(Icons.apple),
+                          label: Text(
+                            'Sign in with Apple',
+                            style: TextStyle(
+                              fontSize: unitHeightValue * 1.3,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+              SizedBox(
+                height: unit * 2,
               ),
-            ),
-            // Padding(
-            //   padding:
-            //       EdgeInsets.fromLTRB(unit * 8, unit * 2, unit * 1, unit * 1),
-            //   child: Row(
-            //     children: [
-            //       Text(
-            //         'Don\'t have an account? ',
-            //         style: TextStyle(
-            //           fontSize: unitHeightValue * 2.3,
-            //         ),
-            //       ),
-            //       GestureDetector(
-            //         onTap: (() {
-            //           Navigator.push(
-            //               context,
-            //               MaterialPageRoute(
-            //                   builder: (context) => const SingupView()));
-            //         }),
-            //         child: Text(
-            //           'Create now !',
-            //           style: TextStyle(
-            //             fontSize: unitHeightValue * 2.3,
-            //           ),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // )
-          ],
+              RichText(
+                text: TextSpan(
+                  text: 'Don\'t have an account? ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: unit * 2.3,
+                  ),
+                  children: [
+                    TextSpan(
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = widget.onClickedSignup,
+                      text: 'Sign Up',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Theme.of(context).colorScheme.secondary),
+                    )
+                  ],
+                ),
+              ),
+              // Padding(
+              //   padding:
+              //       EdgeInsets.fromLTRB(unit * 8, unit * 2, unit * 1, unit * 1),
+              //   child: Row(
+              //     children: [
+              //       Text(
+              //         'Don\'t have an account? ',
+              //         style: TextStyle(
+              //           fontSize: unitHeightValue * 2.3,
+              //         ),
+              //       ),
+              //       GestureDetector(
+              //         onTap: (() {
+              //           Navigator.push(
+              //               context,
+              //               MaterialPageRoute(
+              //                   builder: (context) => const SingupView()));
+              //         }),
+              //         child: Text(
+              //           'Create now !',
+              //           style: TextStyle(
+              //             fontSize: unitHeightValue * 2.3,
+              //           ),
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // )
+            ],
+          ),
         ),
       ),
     );
